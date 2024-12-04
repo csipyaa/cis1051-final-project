34409f904dd5743b630ecf6598e1ffc6622f141f
csipyaa <iammarcusng123@gmail.com>
2024-12-04 09:48:35 -0500

    Add files via upload

---
+++ webpage/script.js
@@ -0,0 +1,11 @@
+// successful password requirements
+// minimum length must be at least 14 characters or more.
+// must include the following at least 1 of the following 
+// * special character; upper-case letter;
+// 
+// 
+// 
+
+function passlength() {
+    
+}
