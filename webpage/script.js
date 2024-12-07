703a4496f3f85d27e434694a30ea1c994217501d
csipyaa <iammarcusng123@gmail.com>
2024-12-07 01:53:44 -0500

    add files via upload
    Initialized Firebase getAuth;
    Added An event Listener For Login Button;

--- webpage/script.js
+++ webpage/script.js
@@ -1,11 +1,37 @@
-// successful password requirements
-// minimum length must be at least 14 characters or more.
-// must include the following at least 1 of the following 
-// * special character; upper-case letter;
-// 
-// 
-// 
+// Import the functions you need from the Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-function passlength() {
-    
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBW2h1bq8fVxf4sQBfo8XMUWOTv8mRQjSo",
+  authDomain: "email-authentication-4f93e.firebaseapp.com",
+  projectId: "email-authentication-4f93e",
+  storageBucket: "email-authentication-4f93e.appspot.com",
+  messagingSenderId: "337861350216",
+  appId: "1:337861350216:web:2900a1d44e0824c7f2597d",
+  measurementId: "G-H1DWTDBSPN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+// Grab input and submit button fields
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const submitButton = document.getElementById('loginButton');
+
+// Event Listener for the login button
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent page refresh
+
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    alert('testing')
+
+});
